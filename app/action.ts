8f0a39abad291ddfce7cc57b63ee08f93c114c6d
Csizmadia László <csimadialacko@gmail.com>
2024-05-08 21:56:57 +0300

    first commit

---
+++ app/action.ts
@@ -0,0 +1,41 @@
+'use server'
+
+import { getServerSession } from "next-auth";
+import prisma from "./lib/db";
+import { authOptions } from "./lib/auth";
+
+export async function postData(formData: FormData) {
+    'use server';
+
+    const Pusher = require("pusher"); //The code const Pusher = require("pusher"); is importing the Pusher library into a JavaScript or Node.js application
+
+    const session = await getServerSession(authOptions);
+    const userMessage = formData.get('message');
+
+    const data = await prisma.message.create({
+        data: {
+            message: userMessage as string,
+            email: session?.user?.email as string,
+        }, 
+        include: {
+            User: {
+                select:{
+                    name: true,
+                    image: true,
+                }
+            }
+        }
+    });
+
+    const pusher = new Pusher({
+        appId: process.env.PUSHER_APP_ID,
+        key: process.env.NEXT_PUBLIC_PUSHER_KEY,
+        secret: process.env.PUSHER_SECRET,
+        cluster: 'eu',
+        useTLS: true,
+    });
+
+    pusher.trigger('chat', 'hello', {
+        message: `${JSON.stringify(data)}\n\n`
+    })
+}
