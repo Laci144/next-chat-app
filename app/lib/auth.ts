146011e8803d74216cab6188e37cef4de540f2eb
Csizmadia László <csimadialacko@gmail.com>
2024-05-10 09:15:33 +0300

    Tab name changed

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -18,5 +18,6 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
     ],
-    allowDangerousEmailAccountLinking: true
+    allowDangerousEmailAccountLinking: true,
+   
 };
